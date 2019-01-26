606d28101266522b61a4c737ef117afedaba51c3
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2019-01-26 21:34:00 +0200

    Final stage has now been implemented in a placeholder manner.

--- Assets/Scripts/Levels/Stage/FinalStageBehaviour.cs
+++ Assets/Scripts/Levels/Stage/FinalStageBehaviour.cs
@@ -6,10 +6,21 @@ namespace DerbyRoyale.Levels
 {
 	public class FinalStageBehaviour : StageBehaviour
 	{
+		#region CONSTANTS
+		public const string STAGE_BEGIN_TRIGGER = "StageBegin";
+		#endregion
+
+
+		#region PUBLIC API
 		public override IEnumerator StartBehaviourSequence()
 		{
-			yield return null;
-			Debug.Log("Final stage!");
+			Debug.Log($"Starting stage {stage.stageNumber}");
+
+			animator.SetTrigger(STAGE_BEGIN_TRIGGER);
+			RaiseStageProgressChanged(StageProgress.Begin);
+			yield return new WaitForSeconds(stage.parameters.stageTimeout);
+
 		}
+		#endregion
 	}
 }
--- Assets/Scripts/Levels/Stage/GenericStageBehaviour.cs
+++ Assets/Scripts/Levels/Stage/GenericStageBehaviour.cs
@@ -7,6 +7,7 @@ namespace DerbyRoyale.Levels
 	public class GenericStageBehaviour : StageBehaviour
 	{
 		#region CONSTANTS
+		public const string STAGE_BEGIN_TRIGGER = "StageBegin";
 		public const string STAGE_COMPLETE_TRIGGER = "StageComplete";
 		public const string STAGE_DESTROY_TRIGGER = "StageDestroy";
 		#endregion
@@ -16,19 +17,24 @@ namespace DerbyRoyale.Levels
 		public override IEnumerator StartBehaviourSequence()
 		{
 			Debug.Log($"Starting stage {stage.stageNumber}");
+
+			animator.SetTrigger(STAGE_BEGIN_TRIGGER);
 			RaiseStageProgressChanged(StageProgress.Begin);
 			yield return new WaitForSeconds(stage.parameters.stageTimeout);
 
 			Debug.Log($"Completing stage {stage.stageNumber}");
-			RaiseStageProgressChanged(StageProgress.CompletedBegin);
+
 			animator.SetTrigger(STAGE_COMPLETE_TRIGGER);
+			RaiseStageProgressChanged(StageProgress.CompletedBegin);
 			yield return new WaitForSeconds(stage.parameters.stageCompleteTimeout);
 			RaiseStageProgressChanged(StageProgress.CompletedEnd);
 
 			Debug.Log($"Stage {stage.stageNumber} completed. Begining detroy timeout.");
+
 			yield return new WaitForSeconds(stage.parameters.stageDestroyTimeout);
 			animator.SetTrigger(STAGE_DESTROY_TRIGGER);
 			RaiseStageProgressChanged(StageProgress.Destroyed);
+
 			Debug.Log($"Stage {stage.stageNumber} destroyed.");
 
 			// Clean up game objects
