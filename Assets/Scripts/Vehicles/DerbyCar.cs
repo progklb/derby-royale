a8892d5ee7e631960cf0d4f67fe43428e0c228b3
Connor Richards <connorrichardsgaming@gmail.com>
2019-01-26 19:19:33 +0200

    Added scene for testing vehicle physics and tweaked vehicle scripts.
    [A] Added Vehicle Testing Scene.
    [C] Changed DerbyCar to only implement driving logic that works best, and removed respective editor fields.
    [I] Tweaked DerbyCar constants for better initial driving feel.
    [C] Updated Dervy Car prefab based on script changes.
    [X] Updated incorrectly spelled project namespace and component menus.

--- Assets/Scripts/Vehicles/DerbyCar.cs
+++ Assets/Scripts/Vehicles/DerbyCar.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
 
-namespace DerbyRoyal.Vehicles
+namespace DerbyRoyale.Vehicles
 {
-    [RequireComponent(typeof(Rigidbody), typeof(MeshCollider), typeof(MeshRenderer))]
-    [RequireComponent(typeof(VehicleController), typeof (MeshFilter))]
-    [AddComponentMenu("Derby Royal/Vehicles/Derby Car")]
+    [RequireComponent(typeof(Rigidbody), typeof(MeshRenderer), typeof(MeshFilter))]
+    [RequireComponent(typeof(VehicleController))]
+    [AddComponentMenu("Derby Royale/Vehicles/Derby Car")]
     public class DerbyCar : MonoBehaviour
     {
         #region NESTED TYPES
@@ -23,29 +23,17 @@ namespace DerbyRoyal.Vehicles
 
 
         #region CONSTANTS
-        private float DEFAULT_ACCELERATION = 100f;
-        private float TURN_RATE = 0.5f;
+        private float DEFAULT_ACCELERATION = 1000f;
+        private float TURN_RATE = 5.0f;
         #endregion
 
 
         #region PROPERTIES
         private Rigidbody rigidBody { get => m_RigidBody ?? (m_RigidBody = GetComponent<Rigidbody>()); }
-        private Vector3 forwardAcceleration { get => Vector3.forward * DEFAULT_ACCELERATION * Time.deltaTime; }
-        private CarAcceleration carAcceleration { get => m_CarAcceleration; }
-        private Vector3 rightTurningRotation { get => Vector3.right * TURN_RATE * Time.deltaTime; }
-        private Vector3 rightTurningTorque { get => transform.up * ((vehicleController.turning * TURN_RATE * Time.deltaTime) * 10000f); }
-        private CarSteering carSteering { get => m_CarSteering; }
+        private Vector3 forwardAcceleration { get => transform.forward * DEFAULT_ACCELERATION * Time.deltaTime; }
+        private Vector3 rightTurningTorque { get => transform.up * (vehicleController.turning * TURN_RATE * Time.deltaTime); }
 
         private VehicleController vehicleController { get => m_VehicleController ?? (m_VehicleController = GetComponent<VehicleController>()); }
-        #endregion
-
-
-        #region EDITOR FIELDS
-        [Space(3), Header("DERBY CAR SETUP"), Space(5)]
-        [SerializeField]
-        private CarAcceleration m_CarAcceleration = CarAcceleration.ByForce;
-        [SerializeField]
-        private CarSteering m_CarSteering = CarSteering.ByRotation;
         #endregion
 
 
@@ -66,26 +54,12 @@ namespace DerbyRoyal.Vehicles
         #region PUBLIC API
         public void AccelerateCar()
         {
-            if (carAcceleration == CarAcceleration.ByForce)
-            {
-                rigidBody.AddForce(forwardAcceleration, ForceMode.Acceleration);
-            }
-            else
-            {
-                rigidBody.AddTorque(forwardAcceleration, ForceMode.Acceleration);
-            }
+            rigidBody.AddForce(forwardAcceleration, ForceMode.Acceleration);
         }
 
         public void TurnCar()
         {
-            if (carSteering == CarSteering.ByRotation)
-            {
-                transform.Rotate(rightTurningRotation);
-            }
-            else
-            {
-                rigidBody.AddRelativeTorque(rightTurningTorque);
-            }
+            rigidBody.AddTorque(rightTurningTorque, ForceMode.Force);
         }
         #endregion
 
