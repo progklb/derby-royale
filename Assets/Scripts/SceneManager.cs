f769b555638ac852504ede53b5d035333f486d64
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2019-01-26 15:37:37 +0200

    Added SceneManager and loading from main menu to game scene and vice versa.

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnitySceneManagement = UnityEngine.SceneManagement;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace DerbyRoyale
+{
+	public class SceneManager : MonoBehaviour
+	{
+		#region TYPES
+		private enum SceneType
+		{
+			MainMenu = 0,
+			GameScene = 1
+		}
+		#endregion
+
+
+	    #region EDITOR FIELDS
+		[SerializeField] private string m_MainMenuScene; 
+		[SerializeField] private string m_GameScene; 
+		#endregion
+
+
+		#region PUBLIC API
+		public void LoadScene(int sceneType)
+		{
+			StartCoroutine(LoadSceneSequence((SceneType)sceneType));
+		}
+
+		public void UnloadScene(int sceneType)
+		{
+			StartCoroutine(UnloadSceneSequence((SceneType)sceneType));
+		}
+		#endregion
+
+
+		#region HELPER FUNCTIONS
+		IEnumerator LoadSceneSequence(SceneType type)
+		{
+			yield return UnitySceneManager.LoadSceneAsync(GetSceneName(type), UnitySceneManagement.LoadSceneMode.Additive);
+		}
+
+		IEnumerator UnloadSceneSequence(SceneType type)
+		{
+			yield return UnitySceneManager.UnloadSceneAsync(GetSceneName(type));
+		}
+		
+		string GetSceneName(SceneType type)
+		{
+			switch (type)
+			{
+				case SceneType.MainMenu:
+					return m_MainMenuScene;
+				case SceneType.GameScene:
+					return m_GameScene;
+				default:
+					return null;
+			}
+		}
+		#endregion
+	}
+}
