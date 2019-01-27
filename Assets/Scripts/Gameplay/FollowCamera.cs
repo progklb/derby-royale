a35a96e0aeeaf59df2693c9ebdcef0caf9851fdf
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2019-01-27 02:19:49 +0200

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Assets/Scripts/Gameplay/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace DerbyRoyale.Gameplay
+{
+	public class FollowCamera : MonoBehaviour
+	{
+		#region EDITOR FIELDS
+		[SerializeField] private Vector3 m_Offset;
+		#endregion
+
+
+		#region PROPERTIES
+		private Transform playerTransform { get => GameManager.instance?.playerInstance?.transform; }
+		#endregion
+
+
+		#region UNITY EVENTS
+		void Update()
+		{
+		}
+		#endregion
+	}
+}
