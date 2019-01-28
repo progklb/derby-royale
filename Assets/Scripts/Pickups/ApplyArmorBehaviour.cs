2b7e79c3533eb3ae5e987e86ebbedf9b78f6d63d
Space Laser Raptor <connorrichardsgaming@gmail.com>
2019-01-28 21:53:51 +0200

    Added all base pickup behaviours, setup spawner, controller and pickup prefabs and added all base pickup prefabs.
    [B] Pickup doesn't spawn relative to the spawner correctly, and trigger doesn't see car object from collider.
    [A] Added ApplyArmorBehaviour, ApplyDamageBehaviour, BoostBehaviour, RepairSelfBehaviour amd SlideBehaviour.
    [A] Added Pickup Controller, Pickup Spawner and default 'quirkily' named Pickup Prefabs.
    [A] Added animations for Derby Pickup prefab.
    [A] Added simple 'Metal' material to project.

---
+++ Assets/Scripts/Pickups/ApplyArmorBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+using DerbyRoyale.Vehicles;
+
+namespace DerbyRoyale.Pickups
+{
+    [AddComponentMenu("Derby Royale/Pickups/Pickup Behaviours/Apply Armor Behaviour")]
+    public class ApplyArmorBehaviour : PickupBehaviour
+    {
+        #region PROPERTIES
+        public float armoredDuration { get => m_ArmoredDuration; }
+        #endregion
+
+
+        #region EDITOR FIELDS
+        [Space(10), Header("ARMOR SETTINGS"), Space(5)]
+        [SerializeField, Range(0.1f, 20f), Tooltip("Adds armor against damage to the vehicle for 'Armored Duration' in seconds.")]
+        private float m_ArmoredDuration = 10f;
+        #endregion
+
+
+        #region PUBLIC API
+        public override void UsePickup(DerbyCar owningDerbyCar)
+        {
+            owningDerbyCar.ApplyCarArmor(armoredDuration);
+        }
+        #endregion
+    }
+}
